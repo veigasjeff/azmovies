118f030feb9b4d7c72d190b9ebd9a34df5954e32
mxplayermovies <110382877+veigasjeff@users.noreply.github.com>
2024-07-23 03:52:06 +0530

    yes update

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import { PageTransition } from '../components/PageTransition';
 import GoogleAnalytics from '@bradgarropy/next-google-analytics';
 import Script from 'next/script';
 
+
 function Application({ Component, pageProps }) {
   return (
     <div className='center'>
