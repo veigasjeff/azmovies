51feb0389a2ff29ab6a1d2ac2f76be70109166ed
mxplayermovies <110382877+veigasjeff@users.noreply.github.com>
2024-07-27 21:49:47 +0530

    FULL SET UP

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://azmovies.vercel.app/',
+    changefreq: 'daily',
+    priority: 1.00,
+    // generateRobotsTxt: true,
+    sitemapSize: 7000,
+  }
---
+++ pages/_document.js
@@ -0,0 +1,142 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+
+class MyDocument extends Document {
+  render () {
+    return (
+      <Html lang='en'>
+        <Head>
+          <link
+            rel='sitemap'
+            type='application/xml'
+            title='Sitemap'
+            href='https://azmovies.vercel.app/sitemap.xml'
+          />
+          <link
+            rel='icon'
+            type='image/x-icon'
+            href='/favicon.ico'
+          />
+          <link
+            rel='apple-touch-icon'
+            sizes='180x180'
+            href='/apple-touch-icon.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/favicon-32x32.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href='/favicon-16x16.png'
+          />
+          <link rel='manifest' href='/site.webmanifest' />
+          <meta name='googlebot' content='index,follow' />
+          <meta name='revisit-after' content='1 days' />
+          <meta
+            name='robots'
+            content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1'
+          />
+          <meta
+            name='keywords'
+            content='azmovies, a to z movies, a-z movies, az movies, watch free movies, watch movies online, download movies, watch full movies, watch hd movies'
+          />
+            {/* <meta
+              name='description'
+              content='A to Z Movies™ - Explore. Discover. Download.'
+            /> */}
+          {/* <link
+            rel='canonical'
+            href='https://azmovies.vercel.app/'
+          /> */}
+          <meta property='og:locale' content='en_US' />
+          <meta property='og:type' content='website' />
+          <meta
+            property='og:title'
+            content='A to Z Movies™ - Explore. Discover. Download.'
+          />
+          <meta
+            property='og:description'
+            content='Welcome to A to Z Movies™ – your go-to spot for free online movies! Watch and enjoy HD streaming, and catch the latest tvshows. Dive into cinema with A to Z Movies™!'
+          />
+          <meta
+            property='og:url'
+            content='https://azmovies.vercel.app/'
+          />  
+          <meta
+            property='og:site_name'
+            content='A to Z Movies - Explore. Discover. Download.'
+          />
+          <meta
+            property='og:image'
+            content='https://azmovies.vercel.app/og_image.jpg'
+          />
+          <meta property='og:image:width' content='1280' />
+          <meta property='og:image:height' content='720' />
+          <meta property='og:image:type' content='image/webp' />
+          <meta
+            name='application-name'
+            content='A to Z Movies - Explore. Discover. Download.'
+          />
+          <meta
+            property='article:modified_time'
+            content='2024-01-01T13:13:13+00:00'
+          />
+          <link
+            rel='sitemap'
+            type='application/xml'
+            title='Sitemap'
+            href='https://azmovies.vercel.app/sitemap.xml'
+          />
+          <meta name='twitter:card' content='summary_large_image' />
+      
+          <meta
+            name='google-adsense-account'
+            content='ca-pub-5527677677744511'
+          />
+          <meta
+            name='google-site-verification'
+            content='BZNZaUyoS1nXyRfa99f4VJ3ABKZUZhkKB0pZ3DU3L8s'
+          />
+          <meta
+            name='facebook-domain-verification'
+            content='du918bycikmo1jw78wcl9ih6ziphd7'
+          />
+          <meta
+            name='dailymotion-domain-verification'
+            content='dmdzuqt3p027t2adn'
+          />
+          <meta name='monetag' content='35a75bbdeae678c82776e64fb78cdac5' />
+          <link
+            rel='stylesheet'
+            href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
+            integrity='sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=='
+            crossorigin='anonymous'
+            referrerpolicy='no-referrer'
+          />
+           
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+          {/* <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-W62BF3BQ"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript> */}
+       
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
