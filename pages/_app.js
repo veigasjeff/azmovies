f4b91087b87cec6da3de478b062b5b2fa92daeb0
mxplayermovies <110382877+veigasjeff@users.noreply.github.com>
2024-08-25 16:11:24 +0530

    ok ads

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ function Application({ Component, pageProps }) {
   return (
     <div className="center">
       <GoogleAnalytics measurementId="G-E1Z24E5B50" />
-     {/* <Script src='../../propler/ads.js' defer /> */}
+     <Script src='../../propler/ads.js' defer />
      {/* <Script src='../../propler/ads2.js' defer /> */}
       
       <PageTransition>
--- public/propler/ads.js
+++ public/propler/ads.js
@@ -1,14 +1,35 @@
+      // // Function to load the ad code
+      // function loadAdCode() {
+      //   var adScript = document.createElement('script');
+      //   adScript.async = true;
+      //   adScript.setAttribute('data-cfasync', 'false');
+      //   adScript.src = '//thubanoa.com/1?z=7762649';
+      //   document.body.appendChild(adScript);
+      // }
+
+      // // Set a timeout to load the ad code after 5 seconds
+      // setTimeout(loadAdCode, 8000); // 5000 milliseconds = 5 seconds
+
+
+      // // <script async="async" data-cfasync="false" src="//thubanoa.com/1?z=7762649"></script>
+
       // Function to load the ad code
-      function loadAdCode() {
-        var adScript = document.createElement('script');
-        adScript.async = true;
-        adScript.setAttribute('data-cfasync', 'false');
-        adScript.src = '//thubanoa.com/1?z=7762649';
-        document.body.appendChild(adScript);
-      }
+function loadAdCode() {
+  var adScript = document.createElement('script');
+  adScript.src = 'https://alwingulla.com/88/tag.min.js';
+  adScript.setAttribute('data-zone', '92408');
+  adScript.async = true;
+  adScript.setAttribute('data-cfasync', 'false');
 
-      // Set a timeout to load the ad code after 5 seconds
-      setTimeout(loadAdCode, 8000); // 5000 milliseconds = 5 seconds
+  // Handle script load errors
+  adScript.onerror = function() {
+    console.error('Failed to load ad script');
+  };
 
+  document.body.appendChild(adScript);
+}
 
-      // <script async="async" data-cfasync="false" src="//thubanoa.com/1?z=7762649"></script>
+// Set a timeout to load the ad code after 15 seconds
+setTimeout(loadAdCode, 10000); // 15000 milliseconds = 15 seconds
+
+      // <script src="https://alwingulla.com/88/tag.min.js" data-zone="92408" async data-cfasync="false"></script>
