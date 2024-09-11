06b3c72821b5861132853357dab85a97fbc08fa3
mxplayermovies <110382877+veigasjeff@users.noreply.github.com>
2024-09-11 13:03:29 +0530

    track

--- pages/_app.js
+++ pages/_app.js
@@ -35,7 +35,7 @@ function Application({ Component, pageProps }) {
 
   return (
     <div className="center">
-      <GoogleAnalytics measurementId="G-65SVDZN562" />
+      <GoogleAnalytics measurementId="G-E1Z24E5B50" />
   
       
       <PageTransition>
