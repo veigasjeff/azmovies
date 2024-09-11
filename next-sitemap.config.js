446df53193af3f408cad8e90657fbde37092a107
mxplayermovies <110382877+veigasjeff@users.noreply.github.com>
2024-09-11 12:56:14 +0530

    final upsate

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://atozmovies.vercel.app/',
+  changefreq: 'daily',
+  priority: 0.9, // Set priority to just below the highest value
+  // generateRobotsTxt: true,
+  sitemapSize: 7000,
+};
